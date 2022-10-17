859ddd534574b36182bf0b59a5ebf4be0ef66dd9
Patrycja Lamik <patrycjalamik94@gmail.com>
2022-10-17 15:48:41 +0200

    zadanie 8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,6 +1,6 @@
 
      import Player from '@vimeo/player';
-     import { throttle } from 'lodash.throttle';
+     import throttle from 'lodash.throttle';
 
      const iframe = document.querySelector('iframe');
      const player = new Player(iframe);
@@ -26,7 +26,4 @@
                 break;
         }
     });
-
-    const playThrottle = throttle(onPlay, 1000);
-
-    player.on('timeupdate', playThrottle);
+    player.on('timeupdate', throttle(onPlay, 1000));
