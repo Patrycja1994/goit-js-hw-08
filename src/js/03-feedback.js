b443d613a460e79a6e460e6ff002d8acb2143579
Patrycja Lamik <patrycjalamik94@gmail.com>
2022-10-17 22:05:35 +0200

    zadanie 8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,45 +1,43 @@
 
- import throttle from "lodash.throttle";
 
- const feedbackForm = document.querySelector(".feedback-form");
+ const feedbackForm = document.querySelector('.feedback-form');
+ const emailInput = document.querySelector('input');
+ const messageInput = document.querySelector('textarea');
+ const submitButton = document.querySelector('button');
 
- const emailInput = document.querySelector("input");
- const messageInput = document.querySelector("textarea");
- const submitButton = document.querySelector("button");
+ const throttle = require('lodash.throttle');
 
-
-function saveData(){
+function saveData() {
     const data = {
         message: messageInput.value,
         email: emailInput.value,
      }; 
 
-    localStorage.setItem("feedback-form-state", JSON.stringify(data));
-};
-
-const trottleData = throttle(saveData, 500);
-
-feedbackForm.addEventListener('input', trottleData);
-
-if (localStorage === "") {
-    emailInput.value = JSON.parse(localStorage.getItem("feedback-form-state")).email;
-    messageInput.value = JSON.parse(localStorage.getItem("feedback-form-state")).message;
+    localStorage.setItem(`feedback-form-state`, JSON.stringify(data));
 }
 
-feedbackForm.addEventListener('submit', handlesubmit);
+feedbackForm.addEventListener('input', throttle(saveData, 500));
 
-function handlesubmit(e) {
-    e.preventDefault();
+if (localStorage !== null) {
+    emailInput.value = JSON.parse(localStorage.getItem(`feedback-form-state`)).email;
+    messageInput.value = JSON.parse(localStorage.getItem(`feedback-form-state`)).message;
+}
+
+feedbackForm.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
     const {
         elements: {message, email}
-    } = e.currentTarget;
+    } = event.currentTarget;
 
     if (message.value === "" || email.value === "") {
-        return alert ("Fill all fields!")
-    } else {
+        return alert ('Fill all fields!');
+    } 
         const result = {message: message.value, email: email.value};
         console.log(result);
-        localStorage.removeItem("feedback-form-state");
-    }
-    
+
+        event.currentTarget.reset();
+        localStorage.removeItem('feedback-form-state');
 }
+
